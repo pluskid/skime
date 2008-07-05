ced55d0e21103534d272afefbafe2ad4207d7525
pluskid <pluskid@gmail.com>
2008-07-05 17:45:22 -0400

    Added symbol and cons type.

---
+++ cons.py
@@ -0,0 +1,18 @@
+class Cons(object):
+    """\
+    The Cons pair of Scheme.
+
+    Cons can be chained to form a list. The Python value None acts as
+    end-of-list.
+
+      Cons(1, Cons(2, None)) <==> '(1 2) <==> '(1 . (2 . ()))
+    """
+    __slots__ = ['car', 'cdr']
+
+    def __init__(self, car, cdr):
+        self.car = car
+        self.cdr = cdr
+
+    def __eq__(self, other):
+        return self.car == other.car and \
+               self.cdr == other.cdr
---
+++ symbol.py
@@ -0,0 +1,29 @@
+class Symbol(object):
+    symbols = weakref.WeakValueDictionary({})
+
+    def __new__(cls, name):
+        """\
+        Get the interned symbol of name. If no found, create
+        a new interned symbol.
+        """
+        if cls.symbols.has_key(name):
+            return cls.symbols[name]
+
+        sym = object.__new__(cls)
+        sym._name = name
+        cls.symbols[name] = sym
+        return sym
+
+    def __eq__(self, other):
+        return self is other
+
+    def get_name(self):
+        return self._name
+    def set_name(self):
+        raise AttributeError("Can't modify name of a symbol.")
+    name = property(get_name, set_name)
+
+    def __str__(self):
+        return "<symbol %s>" % self._name.__repr__()
+    def __repr__(self):
+        return self.__str__()
